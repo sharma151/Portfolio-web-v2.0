a17aa5c6c2d523889794c1bd2c072ef820a08052
sharma151 <sharmasaurav150503@gmail.com>
2025-04-23 11:42:25 +0545

    Portfolio-v2.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      {/* <button className="bg-gradient-to-r from-blue-500 to-purple-500 text-white p-4 rounded-lg hover:from-pink-500 hover:to-yellow-500 transition-all">
+        Hover Me
+      </button> */}
+    </div>
+  );
+}
+
+export default App;
