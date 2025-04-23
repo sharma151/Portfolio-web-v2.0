ac253047f28d696734c1da33b7f79212b52e050a
sharma151 <sharmasaurav150503@gmail.com>
2025-04-23 22:26:34 +0545

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="h-screen flex justify-center items-center">
-      {/* <button className="bg-gradient-to-r from-blue-500 to-purple-500 text-white p-4 rounded-lg hover:from-pink-500 hover:to-yellow-500 transition-all">
+      <button className="bg-gradient-to-r from-blue-500 to-purple-500 text-white p-4 rounded-lg hover:from-pink-500 hover:to-yellow-500 transition-all">
         Hover Me
-      </button> */}
+      </button>
     </div>
   );
 }
