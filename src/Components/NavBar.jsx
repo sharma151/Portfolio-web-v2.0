1035dbb632e377925d3d12f357cd346cf27afa1e
sharma151 <sharmasaurav150503@gmail.com>
2025-04-24 16:37:03 +0545

    navbar on hover transition added , footer component updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,3 @@
-// import{ useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   AiFillStar,
@@ -10,65 +9,58 @@ import { CgGitFork, CgFileDocument } from "react-icons/cg";
 import logo from "../Assets/logo.png";
 
 function NavBar() {
-  // const [menuOpen, setMenuOpen] = useState(false);
-  // const [scrolled, setScrolled] = useState(false);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     setScrolled(window.scrollY >= 20);
-  //   };
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, []);
-
   return (
-    <nav className="border-red-950 border p-3 items-center">
+    <nav className="border p-3 items-center">
       <div className="flex justify-between">
         <Link to="/" className="flex items-center gap-2 ">
           <img src={logo} alt="brand" className="h-10 " />
         </Link>
 
         {/* Toggle Button for Mobile */}
-        <button className="md:hidden flex flex-col gap-1">
+        {/* <button className="md:hidden flex flex-col gap-1">
           <span className="block h-0.5 w-6 bg-black"></span>
           <span className="block h-0.5 w-6 bg-black"></span>
           <span className="block h-0.5 w-6 bg-black"></span>
-        </button>
+        </button> */}
 
         {/* Nav Links */}
-        <div className="item-center">
-          <ul className=" flex gap-8">
-            <li>
+        <div className="">
+          <ul className="flex gap-8 text-lg">
+            <li className="group relative">
               <Link
                 to="/"
                 className="flex items-center gap-1 hover:text-blue-600 transition"
               >
                 <AiOutlineHome /> Home
               </Link>
+              <span className="absolute left-0  h-0.5 w-0 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
             </li>
-            <li>
+            <li className="group relative">
               <Link
                 to="/about"
                 className="flex items-center gap-1 hover:text-blue-600 transition"
               >
                 <AiOutlineUser /> About
               </Link>
+              <span className="absolute left-0  h-0.5 w-0 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
             </li>
-            <li>
+            <li className="group relative">
               <Link
                 to="/projects"
                 className="flex items-center gap-1 hover:text-blue-600 transition"
               >
                 <AiOutlineFundProjectionScreen /> Projects
               </Link>
+              <span className="absolute left-0  h-0.5 w-0 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
             </li>
-            <li>
+            <li className="group relative">
               <Link
                 to="/resume"
                 className="flex items-center gap-1 hover:text-blue-600 transition"
               >
                 <CgFileDocument /> Resume
               </Link>
+              <span className="absolute left-0  h-0.5 w-0 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
             </li>
             <li>
               <a
