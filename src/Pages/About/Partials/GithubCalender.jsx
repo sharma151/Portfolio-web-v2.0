9fefa5034756d6508c1fcc1da7896b292b5ce660
sharma151 <sharmasaurav150503@gmail.com>
2025-04-27 19:09:10 +0545

    githubcalendar added

---
+++ src/Pages/About/Partials/GithubCalender.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+
+function Github() {
+  return (
+    <div className="flex flex-col items-center pb-4">
+      <h1 className="text-3xl font-bold pb-5 text-center text-white">
+        Days I <span className="text-purple-500">Code</span>
+      </h1>
+
+      <GitHubCalendar
+        username="sharma151"
+        blockSize={15}
+        blockMargin={5}
+        color="#c084f5"
+        fontSize={16}
+        style={{ color: "white" }}
+      />
+    </div>
+  );
+}
+
+export default Github;
