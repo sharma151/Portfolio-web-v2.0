6e4123b6d1580eff0a91011772d955a422c719ee
sharma151 <sharmasaurav150503@gmail.com>
2025-05-09 22:25:16 +0545

    framer-motion animation added

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,23 +1,34 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from "react";
+import { motion } from "framer-motion";
 import { AiFillGithub } from "react-icons/ai";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
-function ProjectCard({ title, image, github, live, info }) {
+function ProjectCard({ title, image, github, live, info, index }) {
   return (
-    <div className="bg-gray-900 rounded-xl border border-gray-700 hover:shadow-lg hover:shadow-purple-500/40 transition duration-300 p-4">
+    <motion.div
+      className=" rounded-xl border border-gray-700 hover:shadow-lg hover:shadow-purple-500/40 transition duration-300 p-4 "
+      initial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, ease: "easeOut", delay: index * 0.2 }}
+      viewport={{ once: false, amount: 0.2 }}
+    >
       <img
         src={image}
         alt={title}
         className="rounded-md mb-4 w-full h-48 object-cover"
       />
-      <h3 className="text-white text-xl font-semibold mb-4">{title}</h3>
+      <h3 className="text-[var(--text-color)] text-xl font-semibold mb-4">
+        {title}
+      </h3>
       <p className="text-gray-400 mb-4">{info}</p>
       <div className="flex justify-between">
         <a
           href={github}
           target="_blank"
           rel="noreferrer"
-          className="flex items-center gap-2 bg-gray-800 text-white px-4 py-2 rounded hover:bg-purple-500 transition border border-purple-600"
+          className="flex items-center gap-2 text-[var(--text-color)] px-4 py-2 rounded transition border border-purple-600"
         >
           <AiFillGithub />
           GitHub
@@ -26,13 +37,13 @@ function ProjectCard({ title, image, github, live, info }) {
           href={live}
           target="_blank"
           rel="noreferrer"
-          className="flex items-center gap-2 bg-purple-600 text-white px-4 py-2 rounded border border-purple-600 hover:bg-purple-700 transition"
+          className="flex items-center gap-2  text-[var(--text-color)] px-4 py-2 rounded border border-purple-600  transition"
         >
           <FaExternalLinkAlt />
           Live
         </a>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
