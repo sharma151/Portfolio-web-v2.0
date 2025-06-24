bc4c5c953a713e0fcb891753d01df972d0122c9d
sharma151 <sharmasaurav150503@gmail.com>
2025-06-25 00:09:30 +0545

    fix: project link added to taskschedular in projectpage

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,63 +1,63 @@
-import FeaturedProjects from "@/Components/FeaturedProjects";
-import React from "react";
+import FeaturedProjects from '@/Components/FeaturedProjects';
+import React from 'react';
 
 const Projects = () => {
   const projects = [
     {
-      title: "My Portfolio Website",
+      title: 'My Portfolio Website',
       image:
-        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       github: null,
       live: null,
-      info: "A sleek and responsive portfolio website built with React.js, styled using Tailwind CSS, and enhanced with smooth animations via Framer Motion. Showcases best practices in React component reusability and clean, maintainable code architecture.",
+      info: 'A sleek and responsive portfolio website built with React.js, styled using Tailwind CSS, and enhanced with smooth animations via Framer Motion. Showcases best practices in React component reusability and clean, maintainable code architecture.',
     },
     {
-      title: "Social media platform",
+      title: 'Social media platform',
       image:
-        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      github: "",
-      live: "",
-      info: "A full-featured social media platform built with React, implementing user authentication, photo posting, user search, and profile management. Utilizes Context API, Redux, React Hooks, component-based architecture, API integration with interceptors, and best practices for scalable, professional-grade development.",
+        'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      github: '',
+      live: '',
+      info: 'A full-featured social media platform built with React, implementing user authentication, photo posting, user search, and profile management. Utilizes Context API, Redux, React Hooks, component-based architecture, API integration with interceptors, and best practices for scalable, professional-grade development.',
     },
     {
-      title: "InfoSnap",
+      title: 'InfoSnap',
       image:
-        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      github: "",
-      live: "",
-      info: "A versatile  InfoSnap generator hub built with React and styled using Sass/SCSS, allowing users to create barcodes, QR codes, and digital business cards by simply entering their data. Designed for ease of use with a clean, responsive interface.",
+        'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      github: '',
+      live: '',
+      info: 'A versatile  InfoSnap generator hub built with React and styled using Sass/SCSS, allowing users to create barcodes, QR codes, and digital business cards by simply entering their data. Designed for ease of use with a clean, responsive interface.',
     },
     {
-      title: "Giphy Search application",
+      title: 'Giphy Search application',
       image:
-        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      github: "",
-      live: "",
-      info: "A Giphy search app that lets users find and browse GIFs using the Giphy API. Built with Firebase authentication for secure user login, it allows users to save and manage their favorite GIFs seamlessly.",
+        'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      github: '',
+      live: '',
+      info: 'A Giphy search app that lets users find and browse GIFs using the Giphy API. Built with Firebase authentication for secure user login, it allows users to save and manage their favorite GIFs seamlessly.',
     },
     {
-      title: "Task Schedular",
+      title: 'Task Schedular',
       image:
-        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      github: "",
-      live: "",
-      info: "A task scheduler application built with TypeScript, React, and Tailwind CSS. Allows users to create, edit, delete, and save tasks based on specific dates. Designed with a clean UI and structured for scalability and maintainability.",
+        'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      github: 'https://github.com/sharma151/Task-schedular',
+      live: 'https://tasksschedular.vercel.app/',
+      info: 'A task scheduler application built with TypeScript, React, and Tailwind CSS. Allows users to create, edit, delete, and save tasks based on specific dates. Designed with a clean UI and structured for scalability and maintainability.',
     },
     {
-      title: "Calculator",
+      title: 'Calculator',
       image:
-        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      github: "",
-      live: "",
-      info: "A modern calculator built with HTML, CSS, and JavaScript featuring a clean, responsive UI. Supports basic arithmetic operations with smooth user interactions and a sleek, intuitive design for an enhanced user experience.",
+        'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      github: '',
+      live: '',
+      info: 'A modern calculator built with HTML, CSS, and JavaScript featuring a clean, responsive UI. Supports basic arithmetic operations with smooth user interactions and a sleek, intuitive design for an enhanced user experience.',
     },
     {
-      title: "Weather application",
+      title: 'Weather application',
       image:
-        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      github: "",
-      live: "",
-      info: "A weather application built with JavaScript, HTML, and CSS that fetches real-time weather data from a free weather API, providing users with up-to-date weather forecasts in a clean and user-friendly interface.",
+        'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      github: '',
+      live: '',
+      info: 'A weather application built with JavaScript, HTML, and CSS that fetches real-time weather data from a free weather API, providing users with up-to-date weather forecasts in a clean and user-friendly interface.',
     },
   ];
   return (
